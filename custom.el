72f93acf1378726a4305dab4bb7538d3015ba3d8
luotuoqingshan <2019hyf@gmail.com>
2024-10-13 20:13:20 -0400

    add custom.el

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+    '("78e6be576f4a526d212d5f9a8798e5706990216e9be10174e3f3b015b8662e27" default)))
+ 
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+  )
